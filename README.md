4700e67eb494bdee8c198a42458ee2599a7ca665
AymenMAATKI <68022315+AymenMAATKI@users.noreply.github.com>
2023-09-19 03:15:20 +0000

    final result

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Medical Data Visualizer
 
-This is the boilerplate for the Medical Data Visualizer project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/medical-data-visualizer
+This is the Medical Data Visualizer project. included in the freeCodeCamp curriculum for obtaining the certification of data analysis with python. 
+as described in the website: "In this project, you will visualize and make calculations from medical examination data using matplotlib, seaborn, and pandas. The dataset values were collected during medical examinations."
+
+
+Instructions for building your project can be found at https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/medical-data-visualizer
--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] =  ((df['weight']/(df['height']/100)**2)>25).astype(int)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df[['cholesterol', 'gluc']]=df[['cholesterol', 'gluc']].replace({1: 0, 2: 1, 3: 1})
 
 # Draw Categorical Plot
 def draw_cat_plot():
+
+  
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
-
-
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    df_cat = pd.melt(df,value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],id_vars=['cardio'])
     
+  
+  # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat_grouped = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    df_cat_grouped.rename(columns={'value': 'variable_value'}, inplace=True)
 
     # Draw the catplot with 'sns.catplot()'
+    g = sns.catplot(
+        x='variable', hue='variable_value', col='cardio',
+        data=df_cat_grouped, kind='count'
+    )
 
+    # Set plot titles and axis labels
+    # Set plot titles and axis labels
+    g.set_axis_labels('variable', 'total')
+    g.set_titles('Cardio = {col_name}')
 
-
-    # Get the figure for the output
-    fig = None
-
-
-    # Do not modify the next two lines
+    # Save the figure
+    fig = g.fig
     fig.savefig('catplot.png')
     return fig
 
 
 # Draw Heat Map
 def draw_heat_map():
-    # Clean the data
-    df_heat = None
 
+  
+    #clean data filter invalid patient data 
+    df_clean = df[
+        (df['ap_hi'] >= df['ap_lo']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+  
     # Calculate the correlation matrix
-    corr = None
+    corr = df_clean.corr()
 
+  
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+  
+    # Set up the Matplotlib figure and axes for the heatmap
+    fig, ax = plt.subplots(figsize=(10, 8))
 
-
-    # Set up the matplotlib figure
-    fig, ax = None
-
+  
     # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr, annot=True, fmt=".1f", cmap="coolwarm", mask=mask, cbar=True, square=True, ax=ax)
+    
 
-
-
-    # Do not modify the next two lines
+    # saving the figure
     fig.savefig('heatmap.png')
     return fig
